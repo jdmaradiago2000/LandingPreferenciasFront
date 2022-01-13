3f59c97a387213413898f1794a39e690735fb82a
DANNIA CASTELLANOS <dannia.castellanos@telefonica.com>
2022-01-13 10:30:06 -0500

    Se realizan ajustes de estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './Components/footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
