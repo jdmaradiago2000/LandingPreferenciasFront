caa031c3dde72ec09e9d2c4f91bd03ecf9c67c7c
jlsorianoro <jose.soriano@fractalia.es>
2022-10-18 09:04:40 -0500

    Lp0-11 Token de seguridad

--- src/app/services/url.ts
+++ src/app/services/url.ts
@@ -1,3 +1,4 @@
+import { prependListener } from 'process';
 import {environment} from '../../environments/environment';
 // _____________________________________________________________
 // *************************PROFILE****************************
@@ -15,3 +16,21 @@ export const ValidateUserInfoExpiredToken= prefix + 'User/ValidateUserInfoExpire
 export const ValidateUserInfoRestartTries= prefix + 'User/ValidateUserInfoRestartTries/';
 export const MensajeTiempoExpirado= prefix + 'User/MensajeTiempoExpirado/';
 export const MensajeIntentoErroneo= prefix + 'User/MensajeIntentoErroneo/';
+
+//TOKEN SERVICES 
+export const generateFirstToken = prefix + 'Tokens/generarPrimerToken';
+export const generateToken = prefix + 'Tokens/generarToken';
+export const generateTokenUntyped = prefix +'Tokens/generarTokenUntyped'
+export const validateToken = prefix + 'Tokens/validarToken';
+export const desactiveToken = prefix + 'Tokens/deshabilitarToken';
+
+export const timeTokenUntyped = prefix+ 'Tokens/tiempoDuracionTokenSinDigitar';
+
+export const timeFirstToken = prefix+ 'Tokens/tiempoDuracionPrimerToken';
+
+export const timeToken = prefix + 'Tokens/tiempoDuracionToken';
+export const amountTryTokenUntyped = prefix + 'Tokens/cantidadIntentosTokenSinDigitar';
+export const amountTryToken = prefix +'Tokens/cantidadIntentosToken';
+export const addBlock = prefix + 'Bloqueo/blockUser';
+
+export const logToken = prefix+ 'Tokens/logToken';
